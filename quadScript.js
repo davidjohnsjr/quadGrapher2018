579ba7d51c183946e465c2b9c49fe5183adb6b1e
David Johhns <electricteaching@gmail.com>
2018-02-27 13:29:13 -0800

    recent upload

--- quadScript.js
+++ quadScript.js
@@ -1,5 +1,5 @@
 //Javascript for drawing and calculating quadratics 2017
-var a, b, c, context, w, h, k=18;
+var a, b, c, context, w, h, k=10;
 
 function init() {
   canvas= document.getElementById("mycanvas");
@@ -7,6 +7,10 @@ function init() {
   w = canvas.width = 600;
   h = canvas.height = 400;
   console.log('canvas is loaded into context',w);
+  canvasOffset = canvas.getBoundingClientRect();
+  offsetX = Math.round(canvasOffset.left),
+  offsetY = Math.round(canvasOffset.top); 
+  canvas.addEventListener("mousemove", doMouseMove, false);
   graphpaper();
 }  // close init
 
@@ -16,30 +20,63 @@ function QF() {
   b = $("#linB").val();
   c = $("#constant").val();
   x1 = (-b+Math.sqrt(b**2-4*a*c))/(2*a);
-  x1 = Math.round(x1*10000)/10000;
+  x1 = Math.round(x1*100)/100;
   x2 = (-b-Math.sqrt(b**2-4*a*c))/(2*a);
+  x2 = Math.round(x2*100)/100;
   $("#solution1").text("X intercept is at "+x1);
   $("#solution2").text("X intercept is at "+x2);
-  console.log(a,b,c);
-  results();
+
+
   graphQuad();
+  results();
+  context.beginPath();
+  context.arc(w/2+x1*k,h/2,5,0,6.28);
+  context.fill();
+
+  context.beginPath();
+  context.arc(w/2+x2*k,h/2,5,0,6.28);
+  context.fill();
 }  // close QF
 
 function results() {
   // finding vertext and displaying symline and yint results
   vX = -(b*1)/(2*a);
   vY = a*Math.pow(vX,2)+b*vX+c*1;
+  vX = vX.toFixed(2);
+  vY = vY.toFixed(2);
   $("#vertex").text("Vertex is at (" + vX+","+vY+")");
-  $("#y-int").text("Y intercept is at (0," + c+")");
-  $("#y-int").text("Y intercept is at (0," + c+")");
+  context.fillStyle = "#032c96";
+  context.beginPath();
+  context.arc(w/2+vX*k,h/2-vY*k,5,0,6.28);
+  context.fill();
 
   // today:  y intercept and corresponding point. 
-  // make a new variable and use $("") with an new paragraphs  
+  // make a new variable and use $("") with an new paragraphs 
+
+  $("#yInt").text("Y intercept is at (0," + c+")");
+  context.beginPath();
+  context.arc(w/2,h/2-c*k,5,0,6.28);
+  context.fill();
+  Xcp = 2*vX;
+  $("#cp").text("Corr. Pt. is at ("+Xcp+","+ c+")");
+  context.beginPath();
+  context.arc(w/2+Xcp*k,h/2-c*k,5,0,6.28);
+  context.fill();
+
+  context.lineWidth= 2;
+  context.strokeStyle= "rgba(0,50,200,.3)";
+  context.setLineDash([10,5]);
+  context.beginPath();
+  context.moveTo(w/2+vX*k,5);
+  context.lineTo(w/2+vX*k,h+5);
+  context.stroke();
+  context.setLineDash([0]);
+
 
 }  // close results()
 
 function graphpaper() {
-  context.lineWidth = 5;
+  context.lineWidth = 3;
   context.beginPath();
   context.moveTo(w/2,0);
   context.lineTo(w/2,h);
@@ -52,7 +89,7 @@ function graphpaper() {
 
   // thin line with a 50% opacity using rgba() 
   context.lineWidth=1;
-  context.strokeStyle="rgba(0,0,255,.5)";
+  context.strokeStyle="rgba(0,0,100,.2)";
 
   //using the direct variation constant, k
   //  here are the vertical and horizontal lines
@@ -91,7 +128,6 @@ function graphQuad () {
     y = c*1+b*x+a*Math.pow(x,2);
     nx =  (w/2-(i+1))/k;
     ny =  c*1+b*nx+a*Math.pow(nx,2);
-    console.log(x,y,nx,ny);
     context.beginPath();
     context.lineWidth = 2;
     context.strokeStyle = "Red";
@@ -100,3 +136,38 @@ function graphQuad () {
     context.stroke();
   }
 }
+
+function zoomIn() {
+  k=k+3;
+  resetCanvas();
+}
+function zoomOut() {
+  k=k-3;
+  resetCanvas();
+}
+
+function resetCanvas() {
+  init();
+  graphQuad();
+  results();
+  QF();
+}
+
+
+
+function doMouseMove(event) {
+    // always know where ther mouse is located
+  resetCanvas();
+  context.fillStyle="yellow";
+  mouseX = event.clientX-offsetX;
+  mouseY = event.clientY-offsetY;
+  pointX = (mouseX-w/2)/k;
+  pointY = a*Math.pow(pointX,2)+b*pointX+c*1;
+  pointX =  pointX.toFixed(2);
+  pointY =  pointY.toFixed(2);
+  //console.log(offsetX,mouseX,mouseY, pointX, pointY, offsetY, offsetX);
+  context.beginPath();
+  context.arc(mouseX, h/2-pointY*k,5,0,2*Math.PI);
+  context.fill(); 
+  $("#point").text("Point on the curve: ("+pointX+","+pointY+")");
+}  // end doMouseMove
